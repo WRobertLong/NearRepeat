07186001f51f3f79c3cc53c6e38257aefce6debb
Robert Long <longrob604@gmail.com>
2020-01-04 18:22:22 +0000

    initial commit for Shiny app

---
+++ R/app.R
@@ -0,0 +1,11 @@
+#  Shiny app for NearRepeat model
+#
+#  Robert Long 2 Dec 2019
+#
+#
+# 
+
+library(shiny)
+ui <- fluidPage()
+server <- function(input, output) {}
+shinyApp(ui = ui, server = server)
