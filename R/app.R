544b49d5f5d7891ee753dd802989db2899bf84b7
Robert Long <longrob604@gmail.com>
2020-01-08 17:49:15 +0000

    add side bar, and ranges for pairs

--- R/app.R
+++ R/app.R
@@ -8,6 +8,9 @@
 #  eg mydata.rda should contain a dataframe
 #  called mydata and this dataframe should
 #  contain X, Y, and time variables
+#
+#  The ranges of sptial pairs and temporal 
+#  distance pairs are displayed after loading
 #  
 #  user also inputs vectors for spatial and 
 #  temporal cut points
@@ -20,41 +23,68 @@ library(shiny)
 ui <- fluidPage(
   
   titlePanel("Near Repeat Dashboard"),
-  mainPanel(
-    fileInput("file", label = ""),
+  
+  sidebarPanel(
+    fileInput("file", label = "Choose a file to load (.rda)", accept = ".rda"),
     textInput('vec1', 'Enter a vector for spatial cut points (comma delimited)', "0,1,2"),
+    
+    # display range of spatial distance pairs
+    tableOutput("spatcut"),
+    hr(),
     textInput('vec2', 'Enter a vector for temporal cut points (comma delimited)', "0,1,2"),
-    actionButton(inputId="plot","Plot"),
-    tableOutput("contents"),
+    
+    # display range of temporal distance pairs
+    tableOutput("tempcut"),
+    hr(),
+    
+    # display the plot
+    actionButton(inputId="plot","Plot")
+  ),
+  mainPanel(
+    
+    # the plot
     plotOutput("NRSTplot")
-    #h4('You entered'),
-    #verbatimTextOutput("oid2")
   )
 )
 
 server <- shinyServer(function(input, output) {
   
-  #output$oid2<-renderPrint({
-  #  sdss <- as.numeric(unlist(strsplit(input$vec1,",")))
-  #  cat("As atomic vector:\n")
-  #  print(sdss)
-  #})
+  # fetch the data
+  data <- reactive({
+    req(input$file)
+    file <- input$file$datapath
+    load(file, envir = .GlobalEnv)
+    dt1 <- get(substr(input$file$name, 1, nchar(input$file$name)-4))
+    dt1 <- dt1[sample(nrow(dt1), 300),]  # for testing only
+    
+  })
+
   
+  # compute range for spatial data pairs
+  output$spatcut <- renderTable({
+    mydf <- data()
+    xy <- cbind(mydf$X, mydf$Y)
+    s_dist <- dist(xy)
+    paste(round(min(s_dist),0), " , ", round(max(s_dist),0))
+  })
+  
+  # compute range for temporal data pairs 
+  output$tempcut <- renderTable({
+    mydf <- data()
+    t_dist <- dist(mydf$date)
+    paste(round(min(t_dist),0), " , ", round(max(t_dist),0))
+  })
+  
+  # build data, do Knox test and output the plot
   observeEvent(input$plot,{
     sdss <- as.numeric(unlist(strsplit(input$vec1,",")))
     tdss <- as.numeric(unlist(strsplit(input$vec2,",")))
-    if (is.null(input$file)) return(NULL)
-  
-    inFile <- isolate({input$file})
-    file <- inFile$datapath
-    load(file, envir = .GlobalEnv)
-    dt <- get(substr(inFile$name, 1, nchar(inFile$name)-4))
-    dt1 <- dt[sample(nrow(dt), 300),]  # for testing only
-    
+
+    dt1 <- data()
+
     nrmod <- NearRepeat(x = dt1$X, y = dt1$Y, time = dt1$date,
                         sds = sdss, tds = tdss)
     
-    # Plot the data
     output$NRSTplot <- renderPlot({
         plot(nrmod)
     })
