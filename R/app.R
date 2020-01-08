4449930db0d2aeedbacdcfacd936b341c50a914b
Robert Long <longrob604@gmail.com>
2020-01-08 12:52:26 +0000

    tidy up

--- R/app.R
+++ R/app.R
@@ -22,7 +22,7 @@ ui <- fluidPage(
 server <- shinyServer(function(input, output) {
   observeEvent(input$plot,{
     if ( is.null(input$file)) return(NULL)
-    inFile <- isolate({input$file })
+    inFile <- isolate({input$file})
     file <- inFile$datapath
     load(file, envir = .GlobalEnv)
     dt <- chicago_be
@@ -36,20 +36,5 @@ server <- shinyServer(function(input, output) {
     })
   })
 })
-
-
-#server <- function(input, output) {
-  #load("data/chicago_be.rda")  
-  #dt <- chicago_be
-  
-  #dt1 <- dt[sample(nrow(dt), 300),]
-  
-  #nrmod <- NearRepeat(x = dt1$X, y = dt1$Y, time = dt1$date,
-  #                    sds = c(0, 10, 20, 30, 40, Inf), tds = c(0,2,4,6))
-  
-  #output$NRSTplot <- renderPlot({
-  #  plot(nrmod)
-  #})
-#}
 
 shinyApp(ui = ui, server = server)
