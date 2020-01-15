e71b14907efabae3dab6d5a4f7269feae3084756
Robert Long <longrob604@gmail.com>
2020-01-15 17:51:14 +0000

    implement C99 standard variable size arrays

--- C/dist.c
+++ C/dist.c
@@ -2,16 +2,20 @@
 # include <stdlib.h>
 # include <math.h>
 
+void print_data(size_t m, size_t n, int array[][m]) { 
+    int i, j; 
+    for (i = 0; i < n; i++) 
+      for (j = 0; j < m; j++) 
+		  printf("%d ", array[i][j]); 
+}
+
 int main() { 
 
     int n = 10, c = 2, i, j, m, dd_len ;
     float *dd ;
-    int *data[n]; 
 	
-    for (i=0; i<n; i++) 
-         data[i] = (int *)malloc(c * sizeof(int)); 
-  
-    // Test data (straight line)
+	int data[n][c] ;
+	
 	for ( i = 0; i < n ; i++) {
 	
 	  data[i][0] = i + 1 ; 
@@ -19,11 +23,8 @@ int main() {
 	
 	}
 	
-    // print the data	
-    //for (i = 0; i <  n; i++) 
-	//for (j = 0; j < c; j++) 
-    //     printf("%d ", data[i][j]); 
-	
+	print_data(n, c, data) ;
+		
 	dd_len = n * (n - 1) / 2 ;
 	dd = malloc( dd_len * sizeof(float)) ;
 	
@@ -35,7 +36,6 @@ int main() {
 		for ( m = i; m < (n - 1); m++ ){
 			// loop over rows of the data
 			// note the initial value of the inner loop counter
- 
 			dd[j] = sqrt(pow((data[m+1][0] - data[i][0]), 2) +  pow((data[m+1][1] - data[i][1]), 2)) ;
 			j++ ;
 		}  
@@ -44,10 +44,6 @@ int main() {
 	//print the distance matrix as a vector
 	for ( i = 0; i < j ; i++)
 		printf("%f ", dd[i]); 
-		
-    // free memory
-	 for (i=0; i<n; i++) 
-         free(data[i]) ; 
 		
    return 0; 
 } 
