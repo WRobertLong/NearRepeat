1512cd47774c8a50f8d11fe208b636750a842528
Robert Long <longrob604@gmail.com>
2020-01-15 16:28:03 +0000

    initial commit for C code to compute distance matrix

---
+++ C/dist.c
@@ -0,0 +1,53 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+
+int main() { 
+
+    int n = 10, c = 2, i, j, m, dd_len ;
+    float *dd ;
+    int *data[n]; 
+	
+    for (i=0; i<n; i++) 
+         data[i] = (int *)malloc(c * sizeof(int)); 
+  
+    // Test data (straight line)
+	for ( i = 0; i < n ; i++) {
+	
+	  data[i][0] = i + 1 ; 
+	  data[i][1] = i + 1 ; 
+	
+	}
+	
+    // print the data	
+    //for (i = 0; i <  n; i++) 
+	//for (j = 0; j < c; j++) 
+    //     printf("%d ", data[i][j]); 
+	
+	dd_len = n * (n - 1) / 2 ;
+	dd = malloc( dd_len * sizeof(float)) ;
+	
+	j = 0 ;
+	
+	for ( i = 0; i < (n - 1); i++ ) {
+		// loop across columns of what will be the lower triangular matrix
+    
+		for ( m = i; m < (n - 1); m++ ){
+			// loop over rows of the data
+			// note the initial value of the inner loop counter
+ 
+			dd[j] = sqrt(pow((data[m+1][0] - data[i][0]), 2) +  pow((data[m+1][1] - data[i][1]), 2)) ;
+			j++ ;
+		}  
+	}
+	
+	//print the distance matrix as a vector
+	for ( i = 0; i < j ; i++)
+		printf("%f ", dd[i]); 
+		
+    // free memory
+	 for (i=0; i<n; i++) 
+         free(data[i]) ; 
+		
+   return 0; 
+} 
