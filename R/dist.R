f872fcb758c48b7d504afe8ca6873fb85c06bdd9
Robert Long <longrob604@gmail.com>
2020-01-15 12:32:03 +0000

    initial commit of dist.R as algo to rewrite in C

--- R/dist.R
+++ R/dist.R
@@ -1,17 +1,43 @@
-
-x <- matrix(c(1,2,3,4,5,6,7,8,9,10), ncol = 2)
-x <- matrix(c(10,9,8,7,6,5,4,3,2,1), ncol = 2)
-x <- matrix(rnorm(10), ncol = 2)
-dist(x, "manhattan")
-dist(x, "euclidean")
-
-
-x <- matrix(c(1,2,3,90,80,70), ncol = 2)
-
-x <- matrix(c(1,1,2,2,3,3), byrow = TRUE, ncol = 2)
-
-x <- matrix(c(3,3,2,2,4,4,5,5), byrow = TRUE, ncol = 2)
-
 #
-# R code to implement dist() function, ultimately to be re-written in C
+# Robert Long  15 Jan 2020
+# R code to implement dist() function, to be re-written in C
 #
+# Function receives a matrix consisting of 2 columns where
+# Each row is a point in 2D space
+# Function returns a lower triangular matrix of distances
+# between all points
+#
+# data matrix = x
+# store result in a vector of length n * (n - 1) / 2 for conversion
+# to lower triangular later = dd
+# current row of data matrix = m
+#
+# 
+# Algorithm:
+# 
+# loop over columns of the LT matrix we will create (index i)
+#   then loop over rows of rows of x (index m)
+# compute distance from m to m + i and store in dd
+
+x <- matrix(c(1,1,2,2,3,3,4,4,5,5), byrow = TRUE, ncol = 2)
+
+n <- nrow(x)
+
+dd <- numeric(n * (n - 1) / 2)
+
+j <- 1
+
+for ( i in 1:(n-1) ) {
+  for ( m in i:(n-1) ) {
+    # loop over rows
+    #i <- 1
+    # calc distance to 
+    
+    dd[j] <- sqrt((x[m + 1, 1] - x[i, 1])^2 + (x[m + 1, 2] - x[i, 2])^2)
+    j <- j + 1
+  }
+}
+
+# test
+vdist <- as.vector(dist(x, "euclidean"))
+all.equal(vdist, dd )
